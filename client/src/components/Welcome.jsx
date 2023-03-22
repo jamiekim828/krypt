97785928a41fa20dbd732acfb130dc21cf50bd4c
jamiekim828 <jamiekim828@gmail.com>
2023-03-22 20:55:19 +0100

    tailwind config

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -106,7 +106,7 @@ export default function Welcome() {
 
             <div className='h-[1px] w-full bg-gray-400 my-2' />
 
-            {true ? (
+            {false ? (
               <Loader />
             ) : (
               <button
