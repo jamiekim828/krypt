2e8e6d140c29e4a729f21c447a64613e86f0f2e8
jamiekim828 <jamiekim828@gmail.com>
2023-03-24 11:30:15 +0100

    working on transactions

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-waffle");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    ropsten: {
+      url: '',
+      accounts: ['']
+    }
+  }
+};
